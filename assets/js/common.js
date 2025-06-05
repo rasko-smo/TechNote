35d80877ffcfa3f0259e65926999233a5b0f7c84
rasko-smo <smo230918@gmail.com>
2025-06-05 11:37:55 +0900

    create common.js about header

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,28 +1,8 @@
-$(function(){
-
-    //header共通部品の作成
-    $("#header").load("/commonConfig/header.html");
-
-    /* JQueryが動作するか確認用 */
-    // alert('スタートしました'); 
-
-    // 以下の処理はCSS上で完結できる処理のためコメントアウトした。
-    // //リンクの上にマウスが乗った時にリンクの文字を斜めにする処理
-    // $('a').on('mouseover', function(){
-    //     $('a').css({
-    //         'font-style':'italic'
-    //     });
-    // });
-
-    // //リンクからマウスを離した時にリンクの文字のフォントを元に戻す処理
-    // $('a').on('mouseout', function(){
-    //     $('a').css({
-    //         'font-style':'normal'
-    //     });
-    // });
+// divブロックをクリックした際に階層直下のaタグのリンクをクリックする処理
+document.querySelectorAll(".linkBlock").forEach(function (div) {
+    div.addEventListener("click", function() {
+        const link = div.querySelector("a");
+        if(link) link.click();
+    });
 });
 
-//JQueryを使用しないheader共通部品の作成
-// fetch("header.html")
-//     .then((response) => response.text())
-//     .then((data) => document.querySelector("#header").innerHTML = data);
